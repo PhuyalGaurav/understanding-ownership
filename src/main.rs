700b521d1628950acc571f70d4cadacf2924bd88
gauravphuyal <phuyalgaurav90@gmail.com>
2024-07-22 20:59:54 +0545

    scopedropping()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    scopedropping();
+}
+
+fn scopedropping() {
+    {
+        let name: String = String::from("Gaurav in the main scope of function: scopedropping()");
+
+        // name is allocated using heap & dropped when the scope ends
+        println!("{name}")
+    }
+    // scope ends name is no longer valid
+    // sprintln!("{name}")
 }
